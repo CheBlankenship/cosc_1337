51717bcf336a55640919a3c8bfcdaa3b5c83c573
CheBlankenship <cnakamura336@gmail.com>
2019-02-04 17:06:08 -0600

    change

--- preview/ch_03/cin_get_example.cpp
+++ preview/ch_03/cin_get_example.cpp
@@ -5,18 +5,29 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-    // char ch;
+    char ch;
 
-    cout << "This program has paused. Press Enter to continue.";
+    int num;
+
+    cout << "entr num .";
+    //cin.get(ch);
+    cin >> num;
+
+    // cin.ignore();
+    cout << "entr char .";
+    //cin.get(ch);
     cin.get(ch);
 
-    cout << "It has paused a second time. Please press Enter again.";
-    ch = cin.get();
+    cout << num << " " << ch<< '\n';
 
-    cout << "It has paused a third time. Please press Enter again.";
-    cin.get();
 
-    cout << "Thank you!";
+    // cout << "It has paused a second time. Please press Enter again.";
+    // ch = cin.get();
+    //
+    // cout << "It has paused a third time. Please press Enter again.";
+    // cin.get();
+    //
+    // cout << "Thank you!";
 
     return 0;
 }
