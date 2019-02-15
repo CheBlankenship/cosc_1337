d0fa1399f8625a3854697e16ee9d77a55802c93d
CheBlankenship <cnakamura336@gmail.com>
2019-02-15 01:08:03 -0600

    update the first function(hardcoded)

--- lab_assignments/lab_2/ch6order.cpp
+++ lab_assignments/lab_2/ch6order.cpp
@@ -7,16 +7,73 @@ using namespace std;
 // Include the class that holds the order data.
 #include "order_info.h"
 
+// Delcarations
+void orderInfo();
 
 int main() {
 
-    
-    OrderInfo order_one;
-    order_one.numOfSpools = 1;
-    order_one.numOfSpoolsInStock = 23;
-    order_one.etceteraCharges = 15;
-
-    order_one.print();
-
+    orderInfo();
     return 0;
+
 }
+
+
+void orderInfo() {
+
+    OrderInfo orderInformation;
+    // # of spools ordered,
+    while (!(std::cout << "Enter the number of spools that got order: " && cin >> orderInformation.numOfOrderedSpools) || !(orderInformation.numOfOrderedSpools > 0))
+    {
+        std::cin.clear();//clear bad input
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Error: Invalid input.\nPlease re-enter(only positive numbers).\n\n";
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+
+    // # of spools in stock,
+    while (!(std::cout << "Enter the number of spools in stock: " && cin >> orderInformation.numOfSpoolsInStock) || !(orderInformation.numOfSpoolsInStock > 0))
+    {
+        std::cin.clear();//clear bad input
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Error: Invalid input.\nPlease re-enter(only positive numbers).\n\n";
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // any special shipping & handling charges above $10 rate.
+    while (!(std::cout << "Enter the number of etcetera charges: " && cin >> orderInformation.etceteraCharges) || !(orderInformation.etceteraCharges > 0))
+    {
+        std::cin.clear();//clear bad input
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Error: Invalid input.\nPlease re-enter(only positive numbers).\n\n";
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // Define a variable that has OrderInfo class.
+    //OrderInfo orderInformation;
+    // Store the user input data.
+    //orderInformation.numOfOrderedSpools = numOfOrderedSpools;
+    //orderInformation.numOfSpoolsInStock = numOfSpoolsInStock;
+    //orderInformation.etceteraCharges = etceteraCharges;
+
+    // Print function helps you see the values.
+    orderInformation.print();
+
+}
+
+
+// void orderSummary() {
+//
+//     // # of spools ready to ship from current stock.
+//     int spoolsReadyToShip;
+//     // # of ordered spools on backorder (if ordered > in stock).
+//     int numOfSpoolsInBackorder;
+//     // Total sales price of portion ready to ship (# of spools ready to ship X $100).
+//     double salesPriceReadyToShip;
+//     // Total of the order ready to ship.
+//     double totalOrderPrice;
+//
+//
+//
+//
+// }
--- lab_assignments/lab_2/order_info.h
+++ lab_assignments/lab_2/order_info.h
@@ -2,14 +2,14 @@
 class OrderInfo
 {
     public:
-        int numOfSpools;
+        int numOfOrderedSpools;
         int numOfSpoolsInStock;
         double etceteraCharges;
 
     // Function to output the information.
     void print()
     {
-        cout << "Num of spools: " << numOfSpools << '\n';
+        cout << "Num of spools: " << numOfOrderedSpools << '\n';
         cout << "Num of spools in stock: " << numOfSpoolsInStock << '\n';
         cout << "Etcetera charges: " << etceteraCharges << '\n';
     }
